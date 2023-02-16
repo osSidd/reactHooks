21148d27825bc2e5abe991d5b4545185b663c428
Osama <107386980+osSidd@users.noreply.github.com>
2023-02-16 20:12:15 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import Navbar from './components/navbar'
+import BookList from './components/booklist'
 function App() {
 
   return (
     <div className="App">
-     
+      <Navbar/>
+      <BookList/>
     </div>
   )
 }
---
+++ src/components/booklist.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class BookList extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>The way of kings</li>
+                    <li>The name of the winds</li>
+                    <li>The final Empire</li>
+                </ul>
+            </div>
+        )
+    }
+}
